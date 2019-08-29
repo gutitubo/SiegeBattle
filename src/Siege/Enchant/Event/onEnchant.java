38cdeca9dfe22a7633163c3b7d8030e34c8d67ea
gutitubo <>
2019-08-29 22:30:36 +0900

    Enchant修正

--- src/Siege/Enchant/Event/onEnchant.java
+++ src/Siege/Enchant/Event/onEnchant.java
@@ -1,6 +1,5 @@
 package Siege.Enchant.Event;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
@@ -9,10 +8,11 @@ public class onEnchant implements Listener {
 
 	@EventHandler
 	public void onEnchanted(EnchantItemEvent e) {
-		Player p = e.getEnchanter();
-		int level = p.getLevel() - e.getExpLevelCost();
-		level = level >= 0 ? level : 0;
-		p.setLevel(p.getLevel() - e.getExpLevelCost());
+//		Player p = e.getEnchanter();
+//		int level = p.getLevel() - e.getExpLevelCost();
+//		level = level >= 0 ? level : 0;
+//		p.setLevel(p.getLevel() - e.getExpLevelCost());
+		e.setExpLevelCost(e.getExpLevelCost());
 	}
 
 }
