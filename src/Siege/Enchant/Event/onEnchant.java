f58295edf3dd8fa85af93950746783d7f891fdb7
BuildTools <unconfigured@null.spigotmc.org>
2019-08-29 22:44:16 +0900

    FIXED

--- src/Siege/Enchant/Event/onEnchant.java
+++ src/Siege/Enchant/Event/onEnchant.java
@@ -1,5 +1,6 @@
 package Siege.Enchant.Event;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
@@ -8,11 +9,10 @@ public class onEnchant implements Listener {
 
 	@EventHandler
 	public void onEnchanted(EnchantItemEvent e) {
-//		Player p = e.getEnchanter();
-//		int level = p.getLevel() - e.getExpLevelCost();
-//		level = level >= 0 ? level : 0;
-//		p.setLevel(p.getLevel() - e.getExpLevelCost());
-		e.setExpLevelCost(e.getExpLevelCost());
+		Player p = e.getEnchanter();
+		int level = p.getLevel() - e.getExpLevelCost();
+		level = level >= 0 ? level : 0;
+		p.setLevel(p.getLevel() - e.getExpLevelCost() + 3);
 	}
 
 }
