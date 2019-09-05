973d468d76c8ebfb0fc028e7afc689fb717b045d
gutitubo <>
2019-09-06 00:11:06 +0900

    BonusChestアイテム受け取りの基本処理を作成

---
+++ src/Siege/BonusChest/BonusChestLoot.java
@@ -0,0 +1,42 @@
+package Siege.BonusChest;
+
+import java.util.Random;
+
+import org.bukkit.inventory.ItemStack;
+
+public class BonusChestLoot {
+
+	public static ItemStack getLoot() {
+		Random rnd = new Random();
+		if (rnd.nextInt(100) < 30) {
+			Random rare = new Random();
+			int rarelity = rare.nextInt(1000);
+			if (rarelity < 1) {
+				return getLegendaryLoot();
+			} else if (rarelity < 100) {
+				return getEpicLoot();
+			} else if ( rarelity < 350) {
+				return getRareLoot();
+			} else {
+				return getNormalLoot();
+			}
+		}
+		return null;
+	}
+
+	public static ItemStack getLegendaryLoot() {
+		return null;
+	}
+
+	public static ItemStack getEpicLoot() {
+		return null;
+	}
+
+	public static ItemStack getRareLoot() {
+		return null;
+	}
+
+	public static ItemStack getNormalLoot() {
+		return null;
+	}
+}
