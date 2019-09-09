650329b534b5d1ae15737d5d65942d83b4cc8277
gutitubo <>
2019-09-09 20:40:46 +0900

    Inventory閉じるときにラピスを渡さなくなる

--- src/Siege/Enchant/Event/onClickedToEnchant.java
+++ src/Siege/Enchant/Event/onClickedToEnchant.java
@@ -4,6 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
@@ -27,6 +28,13 @@ public class onClickedToEnchant implements Listener {
 	public void onOpenInventory(InventoryOpenEvent e) {
 		if (e.getInventory().getType().equals(InventoryType.ENCHANTING)) {
 			e.getInventory().setItem(1, new ItemStack(Material.LAPIS_LAZULI, 64));
+		}
+	}
+
+	@EventHandler
+	public void onCloseInventory(InventoryCloseEvent e) {
+		if (e.getInventory().getType().equals(InventoryType.ENCHANTING)) {
+			e.getInventory().setItem(1, new ItemStack(Material.AIR));
 		}
 	}
 
