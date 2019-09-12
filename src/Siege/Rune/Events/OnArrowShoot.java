39f8d9f0513d84a81bb906c6b6d68423bd5863e1
gutitubo <>
2019-09-13 02:06:43 +0900

    テスト用の記述を追加

--- src/Siege/Rune/Events/OnArrowShoot.java
+++ src/Siege/Rune/Events/OnArrowShoot.java
@@ -1,6 +1,7 @@
 package Siege.Rune.Events;
 
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,30 +10,48 @@ import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import Siege.Rune.Runes;
 import Siege.SiegeCore.SiegeGame;
 import Siege.SiegePlayer.SiegePlayer;
 
 public class OnArrowShoot implements Listener {
-	
+
 	@EventHandler
 	public void onShoot(ProjectileLaunchEvent e) {
 		e.getEntity().getShooter();
 		if (!(e.getEntity() instanceof Arrow)) return;
 		if (!(e.getEntity().getShooter() instanceof Player)) return;
-		
+
 		Player str = (Player) e.getEntity().getShooter();
 		Arrow arw = (Arrow) e.getEntity();
-		
+
 		SiegeGame game = Siege.SiegeBattleMain.siegeBattleMain.getGame();
 		if (game == null) return;
-		
+
 		if (!game.isSiegePlayer(str)) return;
 		SiegePlayer sp = game.getSiegePlayer(str);
-		
+
+		/* === テスト用に実装 TODO 実装版からは消す === */
+		if (arw.isCritical()) arw.setGravity(false);
+		BukkitRunnable arwRun = new BukkitRunnable() {
+			int count = 0;
+			@Override
+			public void run() {
+				// TODO 自動生成されたメソッド・スタブ
+				arw.getWorld().spawnParticle(Particle.CRIT_MAGIC, arw.getLocation(), 1, 0, 0, 0);
+				count ++;
+				if (count > 200) {
+					arw.remove();
+					this.cancel();
+				}
+			}
+		};
+		arwRun.runTaskTimer(Siege.SiegeBattleMain.siegeBattleMain, 0, 1);
+
 		/* === 屋をハナッタナオロアｋジョイラｊコ === */
-		
+
 		if (sp.hasRune(Runes.MAGIC_MAGICARROW)) {
 			ItemStack off = str.getInventory().getItemInOffHand();
 			if (off != null && (off.getType().equals(Material.POTION) || off.getType().equals(Material.SPLASH_POTION))) {
@@ -44,6 +63,6 @@ public class OnArrowShoot implements Listener {
 				str.getInventory().setItemInOffHand(new ItemStack(Material.GLASS_BOTTLE));
 			}
 		}
-	} 
-	
+	}
+
 }
